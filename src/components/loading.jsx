fcb2c0236a054ab8c6da7377ee1a71529431651c
Steve Kinney <hello@stevekinney.net>
2022-11-17 00:07:37 -0600

    Add background to loading indicator

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,7 +1,8 @@
 const Loading = ({ loading = true }) => {
   if (!loading) return null;
+
   return (
-    <div className="flex items-center w-full h-full text-lg font-bold animate-pulse place-content-center text-primary-900">
+    <div className="flex items-center w-full h-full text-lg font-bold animate-pulse place-content-center bg-primary-200 text-primary-900">
       Loading…
     </div>
   );
