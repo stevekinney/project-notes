bee541e430662a11b92a42a59e5bd172500a3ffb
Steve Kinney <hello@stevekinney.net>
2022-11-17 00:19:00 -0600

    Back the default background ligher

--- src/components/application.jsx
+++ src/components/application.jsx
@@ -10,7 +10,7 @@ const Application = () => {
 
   return (
     <main className="flex items-center w-full h-full shadow-2xl overflow-none place-content-center bg-slate-700">
-      <div className="h-[80%] w-5/6 rounded-2xl border-4 border-primary-800 bg-primary-800">
+      <div className="h-[80%] w-5/6 rounded-2xl border-4 border-primary-800 bg-primary-100">
         <div className="\rounded-2xl flex h-full w-full rounded-2xl ">
           <NotesList
             notes={notes}
--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -2,7 +2,7 @@ const Loading = ({ loading = true }) => {
   if (!loading) return null;
 
   return (
-    <div className="flex items-center w-full h-full text-lg font-bold animate-pulse place-content-center bg-primary-200 text-primary-900">
+    <div className="flex items-center w-full h-full text-lg font-bold animate-pulse place-content-center text-primary-900">
       Loading…
     </div>
   );
