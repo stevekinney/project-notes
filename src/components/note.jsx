5f87e10b3e4f220e192f4a7686e3a4587408a7b1
Steve Kinney <hello@stevekinney.net>
2022-11-17 00:12:22 -0600

    Load the editor using suspense
    \

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,7 +1,17 @@
-import Editing from './edit';
+import { lazy, Suspense } from 'react';
+import { simulateNetwork } from '../lib/sleep';
+import Loading from './loading';
+
+const Editing = lazy(() => simulateNetwork(import('./edit')));
 
 const NoteContent = ({ note, setNotes, isEditing }) => {
-  if (isEditing) return <Editing note={note} setNotes={setNotes} />;
+  if (isEditing) {
+    return (
+      <Suspense fallback={<Loading />}>
+        <Editing note={note} setNotes={setNotes} />
+      </Suspense>
+    );
+  }
   return (
     <div className="flex flex-col h-full gap-4 p-4 overflow-y-scroll">
       <h2>{note.title}</h2>
